a62cfe7d7e79c6aec55d3c4af8b4ed0d6c1e5f90
Leo Sjöberg <leo.sjoberg@gmail.com>
2022-09-20 19:08:29 +0300

    Remove machine types as they are unused

--- main.go
+++ main.go
@@ -100,13 +100,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "KindCluster")
 		os.Exit(1)
 	}
-	if err = (&controllers.KindMachineReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "KindMachine")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
